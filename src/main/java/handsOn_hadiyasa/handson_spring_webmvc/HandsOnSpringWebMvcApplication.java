ec153bdaa2d4fed0e11130c5e81980db896d363a
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-11-15 13:39:47 +0700

    Initial Commit

---
+++ src/main/java/handsOn_hadiyasa/handson_spring_webmvc/HandsOnSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package handsOn_hadiyasa.handson_spring_webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handsOn_hadiyasa/handson_spring_webmvc/HandsOnSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package handsOn_hadiyasa.handson_spring_webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
